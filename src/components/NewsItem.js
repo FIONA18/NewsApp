02198ec0f0b44b008b903dd791f0a7ae1c78fac9
FIONA18 <32681518+FIONA18@users.noreply.github.com>
2023-10-14 21:45:29 +0530

    feat: Updated News.js and NewItems.js
    Used a card component from Bootstrap to display top news headlines
    Looped through an array in JSX to display News Items from state

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description, imageUrl, newsURL}= this.props
     return (
       <div>
-        This is a news Item Component.
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={newsURL} className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>
     )
   }
